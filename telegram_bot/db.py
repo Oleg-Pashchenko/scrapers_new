1ea874c8f067ef3dd6e5563046b1edb9bf5fbdd4
olehandro <odpash.itmo@gmail.com>
2023-09-01 17:14:49 +0300

    added enviroment files

--- telegram_bot/db.py
+++ telegram_bot/db.py
@@ -20,45 +20,39 @@ import random
 import os
 import psycopg2
 from typing import Tuple
+import dotenv
+
+dotenv.load_dotenv()
 
 
-def get_items() -> Tuple[Item, Item]:
+def get_items() -> (Item, Item):
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-    )
+    )  # Установление соединения с базой данных
     cur = conn.cursor()
 
-    # Запрос к базе данных для получения непроиндексированных товаров Unas, не содержащих указанные подстроки
-    cur.execute("""
-        SELECT u.id, u.name, u.code, u.image, u.link, o.id, o.name, o.code, o.image, o.link
-        FROM unas u
-        JOIN ozon o ON u.id = o.unas_id
-        WHERE u.is_edited = FALSE
-          AND u.link NOT LIKE ANY (ARRAY['%semena-i-posadochnyy-material%', '%tovary-dlya-otdyha%', '%tovary-dlya-sada%']);
-    """)
+    # Запрос к базе данных для получения непроиндексированных товаров Ozon
+    cur.execute("SELECT * FROM ozon WHERE is_edited=FALSE;")
     records = cur.fetchall()
-
-    if not records:
-        conn.close()
-        return None, None  # Если нет подходящих записей, вернуть None
-
     record = random.choice(records)  # Выбор случайной записи из результатов запроса
-    print(record)
-    time.sleep(3000)
-    unas_item = Item(
-        id=record[0], name=record[1], code=record[2], image=record[3], link=record[4]
-    )
     ozon_item = Item(
-        id=record[5], name=record[6], code=record[7], image=record[8], link=record[9]
+        id=record[0], name=record[2], image=record[3], code=record[4], link=record[5]
     )
-    print(unas_item)
-    print(ozon_item)
-    time.sleep(300)
-    conn.close()
-    return unas_item, ozon_item
+
+    # Запрос к базе данных для получения информации о товаре Unas, связанном с выбранным товаром Ozon
+    cur.execute("SELECT * FROM unas WHERE id=%s;", (record[1],))
+    record = cur.fetchone()
+    unas_item = Item(
+        id=record[0], name=record[1], image=record[3], code=record[2], link=record[4]
+    )
+    conn.close()  # Закрытие соединения с базой данных
+    if 'semena-i-posadochnyy-material' in unas_item.link or\
+            'tovary-dlya-sada' in unas_item.link or 'tovary-dlya-otdyha' in unas_item.link:
+        return get_items()
+    return unas_item, ozon_item  # Возвращение пары товаров
 
 
 # Функция для установки статуса товара и записи статуса в базу данных
@@ -89,3 +83,6 @@ def set_status(status, ozon_id):
     conn.commit()
 
     conn.close()  # Закрытие соединения с базой данных
+
+
+get_items()
